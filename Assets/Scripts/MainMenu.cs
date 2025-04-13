26ec92a0d4ad74dfa94c2c97cf671f392d741a10
EM1103 <edleenmac10@gmail.com>
2025-04-13 23:22:38 +0400

    Added menu.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main_game"); // Replace with your actual scene name
+    }
+}
